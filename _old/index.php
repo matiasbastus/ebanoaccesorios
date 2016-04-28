3eb376ccdd5d3f2bef18df14605139120db62925
matiasbastus <matiasbastus@gmail.com>
2016-04-28 15:24:49 -0300

    Nuevo en construccion

---
+++ _old/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="es">
+<head><meta http-equiv="Content-Type" content="text/html; charset=gb18030">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Ebano :: Accesorios de Plata y Acero">
+	<meta name="author" content="Ebano Accesorios de Plata y ACero - http://ebanoaccesorios.com.ar">
+	<!--Para que las páginas se muestren correctamente y el zoom funcione bien en los dispositivos móviles-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keywords" content="ebano,accesorios,plata,acero,joyas">
+	<title>Ebano :: Accesorios de Plata y Acero</title>
+	<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon"/>
+	<style>
+		h1   {text-align:center;font-family:'Pacifico', cursive; font-size: 300%;color: #C7C7C7;}
+		h2   {text-align:center;font-family:'Pacifico', cursive; font-size: 100%;color: #C7C7C7;}
+	</style>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.5&appId=805385206241759";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+	</script>
+
+</head>
+<body>
+	<div style="text-align:center;">
+	<table align="center">
+		<tr><td><h1>En Construcci&oacute;n</h1></td></tr>
+		<tr><td><img width="250em" src="img/logo_ebano.png"/></td></tr>
+		<tr><td width="30%">
+				<div class="fb-page" data-href="https://www.facebook.com/EbanoPlataYAcero" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+					<div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/EbanoPlataYAcero"><a href="https://www.facebook.com/EbanoPlataYAcero">Ebano Accesorios de Plata y Acero</a></blockquote>
+					</div>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<td width="50%"><a href="mailto:contacto@ebanoaccesorios.com.ar"><img style="margin-top: 10px;" width="50em" src="img/mail.png"/></a></td>
+		</tr>
+	</table>
+	</div>
+</body>
